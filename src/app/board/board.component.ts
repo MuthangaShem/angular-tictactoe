d61486216c3365bc815b505bab68f4bec2f05ff2
muthangashem <muthangashem@gmail.com>
2019-09-28 17:23:10 +0300

    add x or o in click event

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -29,4 +29,11 @@ export class BoardComponent implements OnInit {
     return this.xIsNext ? 'X' : 'O';
   }
 
+  makeMove(idx: number) {
+    console.log('Clicked: ', this.squares[idx]);
+    if (!this.squares[idx]) {
+      this.squares.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    }
+  }
 }
