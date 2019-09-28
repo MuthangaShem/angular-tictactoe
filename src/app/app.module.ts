1c8681bc8bf2f83c6b1adbcf7262a893e17f4035
muthangashem <muthangashem@gmail.com>
2019-09-28 23:03:03 +0300

    add material design and cusotm styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SquareComponent } from './square/square.component';
@@ -14,7 +16,8 @@ import { BoardComponent } from './board/board.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
